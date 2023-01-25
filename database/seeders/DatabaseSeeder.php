b97cf3a09b88d8aa0992847d4d128ba5e6c3b35e
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-25 15:50:34 +0300

    Fills the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +19,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Category::factory(12)->create();
+        $posts = Post::factory(50)->create();
+        $tags = Tag::factory(100)->create();
+        Comment::factory(200)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach($posts as $post) {
+            foreach($tags as $tag) {
+                if (rand(1, 20) == 10) {
+                    $post->tags()->attach($tag->id);
+                }
+            }
+        }
     }
 }
