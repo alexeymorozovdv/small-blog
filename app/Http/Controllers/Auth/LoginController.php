ae9a4d120439d630442410cefcd116c13cb7692c
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-26 14:48:37 +0300

    Adds a check for a remember key in the LoginController's request

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,7 +39,7 @@ class LoginController extends Controller
 
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             return redirect()
                 ->route('user.index')
                 ->with('success', 'You have successfully login');
