5efa22dfaeb0b3878a8bccd2343ec158b5d9a6a2
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-26 14:48:01 +0300

    Adds remember me checkbox and forgot a password link to the login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,8 +12,19 @@
             <input type="text" class="form-control" name="password" placeholder="Password"
                    required maxlength="255" value="">
         </div>
-        <div class="form-group mt-4">
-            <button type="submit" class="btn btn-info text-white">Login</button>
+        <div class="form-group form-check mt-3">
+            <input type="checkbox" class="form-check-input" name="remember" id="remember">
+            <label class="form-check-label" for="remember">
+                Remember me
+            </label>
+        </div>
+        <div class="d-flex flex-row justify-content-between">
+            <div class="form-group mt-3">
+                <button type="submit" class="btn btn-info text-white">Login</button>
+            </div>
+            <div class="mt-3">
+                <a href="{{ route('auth.forgot-form') }}">Forgot a password?</a>
+            </div>
         </div>
     </form>
 @endsection
