ae86aa28588a2707ecb2ebb279d895aca27f7509
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-26 14:23:39 +0300

    Adds forgot and reset password routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,4 +32,12 @@ Route::group(['as' => 'auth.', 'prefix' => 'auth'] , function () {
         ->name('auth');
     Route::post('logout', [LoginController::class, 'logout'])
         ->name('logout');
+    Route::get('forgot-password', [ForgotPasswordController::class, 'form'])
+        ->name('forgot-form');
+    Route::post('forgot-password', [ForgotPasswordController::class, 'mail'])
+        ->name('forgot-mail');
+    Route::get('reset-password/token/{token}/email/{email}', [ResetPasswordController::class, 'form'])
+        ->name('reset-form');
+    Route::post('reset-password', [ResetPasswordController::class, 'reset'])
+        ->name('reset-password');
 });
