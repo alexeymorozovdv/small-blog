39053bd901331d8dffb2a2b6537bb7844c94cea7
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-26 17:06:12 +0300

    Cleanup code (deletes unused imports)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Traits\HasRolesAndPermissions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
