440245bafac1f92dd104b5070663282d6c275437
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-26 17:47:12 +0300

    Creates a main index page and index controller

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', IndexController::class)->name('index');
 
 // Auth
 Route::group(['as' => 'auth.', 'prefix' => 'auth'] , function () {
