16c25b392e01555f2d770226300d3f14096610a5
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-26 17:47:31 +0300

    Adds links to home page in the navbar

--- resources/views/layout/_navbar.blade.php
+++ resources/views/layout/_navbar.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-light border-opacity-10 border border-dark rounded">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Blog</a>
+        <a class="navbar-brand" href="{{ route('index') }}">Blog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarScroll">
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="{{ route('index') }}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
