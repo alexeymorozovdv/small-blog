fe1b9e46094bb4b34afe1fd7b0c5269ca5f2e70a
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-27 15:04:18 +0300

    Change footer position and link to all posts in the navbar

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="text-center text-lg-start bg-light text-muted mt-5 w-100 text-center mx-auto ">
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+<footer class="text-center text-lg-start text-muted mb-0 text-center bottom-0">
+    <div class="text-center p-4 px-5" style="background-color: rgba(0, 0, 0, 0.05);">
         © 2023 Copyright:
         <a class="text-reset fw-bold" href="#">Laravel blog</a>
     </div>
--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -10,9 +10,9 @@
     @vite(['resources/sass/app.js'])
      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="container ">
+<body class="container min-vh-100 d-flex flex-column">
     @include('layout._navbar')
-    <div class="container mt-5">
+    <div class="container mt-5 flex-grow-1 flex-shrink-1">
         <div class="row">
             @include('layout.sidebar._sidebar')
             @include('layout._flash')
