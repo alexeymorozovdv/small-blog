54f99263c691d8574ad4c81ba1d6582f4f7662ad
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-27 17:18:27 +0300

    Changes a type of password filed in the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                    required maxlength="255" value="{{ old('email') ?? '' }}">
         </div>
         <div class="form-group mt-2">
-            <input type="text" class="form-control" name="password" placeholder="Password"
+            <input type="password" class="form-control" name="password" placeholder="Password"
                    required maxlength="255" value="">
         </div>
         <div class="form-group form-check mt-3">
