f237324877fa4c631fc69da833ffc6dfb6b8b0d7
Alexey Morozov <alexeymorozov.dev@gmail.com>
2023-01-27 12:53:18 +0300

    Adds root categories children to sidebar

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
@@ -24,12 +25,33 @@ class Category extends Model
     }
 
     /**
+     * Associating the Category model with the Category model, allows you to get all child categories of the
+     * current category
+     *
+     * @return HasMany
+     */
+    public function children(): HasMany
+    {
+        return $this->hasMany(Category::class, self::PARENT_ID_COLUMN);
+    }
+
+    /**
+     * Linking the Category model to the Category model, allows you to get the parent of the current category
+     *
+     * @return BelongsTo
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, self::PARENT_ID_COLUMN);
+    }
+
+    /**
      * Returns a collection with root categories
      *
      * @return Collection
      */
     public static function roots(): Collection
     {
-        return self::where(self::PARENT_ID_COLUMN, 0)->get();
+        return self::where(self::PARENT_ID_COLUMN, 0)->with('children')->get();
     }
 }
--- resources/views/layout/sidebar/_categories.blade.php
+++ resources/views/layout/sidebar/_categories.blade.php
@@ -3,6 +3,17 @@
         @foreach($items as $item)
             <li>
                 <a href="{{ route('blog.category', ['category' => $item->slug]) }}">{{ $item->name }}</a>
+                @if ($item->children->count())
+                    <ul>
+                        @foreach($item->children as $child)
+                            <li>
+                                <a href="{{ route('blog.category', ['category' => $child->slug]) }}">
+                                    {{ $child->name }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
             </li>
         @endforeach
     </ul>
